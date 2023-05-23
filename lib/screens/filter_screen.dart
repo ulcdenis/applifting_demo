ab2c7d61a268c2b44849777fb49b2245701a4862
Denis Ulč <ulcdenis@email.cz>
2023-05-23 17:23:58 +0200

    filter screen with filtering function on launches

---
+++ lib/screens/filter_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key});
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  late final SharedPreferences prefs;
+  List<int> years = [];
+  List<int> selectedYears = [];
+
+  void getYears() {
+    int initialYear = 2006;
+    int todayYear = DateTime.now().year;
+
+    for (int index = 0; index < todayYear - initialYear + 1; index++) {
+      years.add(initialYear + index);
+    }
+  }
+
+  void setPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final List<String>? items = prefs.getStringList('yearFilter');
+    if (items != null) {
+      for (var element in items) {
+        setState(() {
+          selectedYears.add(int.parse(element));
+        });
+      }
+    }
+  }
+
+  void saveFilter() async {
+    List<String> strList = selectedYears.map((i) => i.toString()).toList();
+    await prefs.remove('yearFilter');
+    await prefs.setStringList('yearFilter', strList);
+    // ignore: use_build_context_synchronously
+    Navigator.pop(context);
+  }
+
+  @override
+  void initState() {
+    setPrefs();
+    getYears();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Filter'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Theme(
+              data: ThemeData().copyWith(dividerColor: Colors.transparent),
+              child: ExpansionTile(
+                iconColor: Colors.black,
+                collapsedIconColor: Colors.black,
+                title: Padding(
+                  padding: const EdgeInsets.all(1),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Launch years',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Text(
+                        selectedYears.length.toString(),
+                        style: const TextStyle(color: Colors.grey, fontStyle: FontStyle.italic),
+                      ),
+                    ],
+                  ),
+                ),
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: years.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              if (selectedYears.contains(years[index])) {
+                                selectedYears.removeWhere((element) => element == years[index]);
+                              } else {
+                                selectedYears.add(years[index]);
+                              }
+                            });
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 5),
+                                child: Text(years[index].toString()),
+                              ),
+                              Icon(
+                                Icons.done,
+                                color: selectedYears.contains(years[index]) ? Colors.black : Colors.transparent,
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(child: Container()),
+            TextButton(
+              onPressed: () => saveFilter(),
+              child: const Text('DONE'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/launches_screen.dart
+++ lib/screens/launches_screen.dart
@@ -1,8 +1,10 @@
 import 'package:applifting_demo/components/custom_future_builder.dart';
 import 'package:applifting_demo/models/launch_model.dart';
+import 'package:applifting_demo/screens/filter_screen.dart';
 import 'package:applifting_demo/screens/launch_detail_screen.dart';
 import 'package:applifting_demo/services/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LaunchesScreen extends StatefulWidget {
   const LaunchesScreen({super.key});
@@ -12,7 +14,14 @@ class LaunchesScreen extends StatefulWidget {
 }
 
 class _LaunchesScreenState extends State<LaunchesScreen> {
+  SharedPreferences? prefs;
+
+  bool searching = false;
+  List<int> yearsFilter = [];
+
   late Future<List<LaunchModel>> launchesFuture = getLaunches();
+  late Future<List<LaunchModel>> searchFuture;
+  TextEditingController searchController = TextEditingController();
 
   Future<List<LaunchModel>> getLaunches() async {
     return await ApiService().getLaunches();
@@ -26,8 +35,48 @@ class _LaunchesScreenState extends State<LaunchesScreen> {
     );
   }
 
+  void onSearch() {
+    searchFuture = search();
+    setState(() {
+      searching = true;
+    });
+  }
+
+  Future<List<LaunchModel>> search() async {
+    List<LaunchModel> tempLaunches = await launchesFuture;
+    List<LaunchModel> search = tempLaunches
+        .where((element) => element.name.toLowerCase().contains(searchController.text.toLowerCase()))
+        .toList();
+    if (yearsFilter.isNotEmpty) {
+      search.removeWhere((element) => !yearsFilter.contains(element.date.year));
+    }
+    return search;
+  }
+
+  void onFilter() async {
+    await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const FilterScreen(),
+      ),
+    );
+    await setFilters();
+  }
+
+  Future<void> setFilters() async {
+    yearsFilter = [];
+    prefs ??= await SharedPreferences.getInstance();
+    final List<String>? items = prefs!.getStringList('yearFilter');
+    if (items != null) {
+      for (var element in items) {
+        yearsFilter.add(int.parse(element));
+      }
+    }
+    onSearch();
+  }
+
   @override
   void initState() {
+    setFilters();
     getLaunches();
     super.initState();
   }
@@ -37,15 +86,42 @@ class _LaunchesScreenState extends State<LaunchesScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Launches'),
-        bottom: const PreferredSize(
-          preferredSize: Size.fromHeight(20),
-          child: TextField(),
+        actions: [
+          Stack(
+            alignment: Alignment.topRight,
+            children: [
+              IconButton(
+                onPressed: () => onFilter(),
+                icon: const Icon(Icons.filter_alt),
+              ),
+              if (yearsFilter.isNotEmpty)
+                const Positioned(
+                  top: 5,
+                  right: 7,
+                  child: Icon(
+                    Icons.circle,
+                    color: Colors.red,
+                    size: 16,
+                  ),
+                )
+            ],
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(20),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: searchController,
+              onChanged: (data) => onSearch(),
+            ),
+          ),
         ),
       ),
       body: Column(
         children: [
           CustomFutureBuilder(
-              future: launchesFuture,
+              future: searching ? searchFuture : launchesFuture,
               builder: (launches) {
                 return Expanded(
                   child: ListView.separated(
@@ -55,7 +131,16 @@ class _LaunchesScreenState extends State<LaunchesScreen> {
                         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                         child: GestureDetector(
                           onTap: () => onLaunchDetail(launches[index].id),
-                          child: Text(launches[index].name),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(child: Text(launches[index].name)),
+                              Text(
+                                '${launches[index].date.day}.${launches[index].date.month}.${launches[index].date.year}',
+                                style: const TextStyle(color: Colors.grey, fontStyle: FontStyle.italic),
+                              )
+                            ],
+                          ),
                         ),
                       );
                     },
