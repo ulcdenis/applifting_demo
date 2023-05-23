ef402d076c9d64e7d9a957d7b53a993b9a6051df
Denis Ulč <ulcdenis@email.cz>
2023-05-23 10:46:05 +0200

    Add initial app

---
+++ android/app/src/main/kotlin/com/example/applifting_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.applifting_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter demo'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
